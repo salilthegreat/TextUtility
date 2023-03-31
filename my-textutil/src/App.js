25647d43d02b5dae1027e9d072775123723be09a
Salil kumar Das <mrnobody@Salils-MacBook-Pro.local>
2023-03-30 18:39:30 -0700

    Dark Mode Enabled

--- my-textutil/src/App.js
+++ my-textutil/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-
+// import TextForm from './components/TextForm';
+import About from './components/About';
 function App() {
   return (
     <>
       <Navbar title="TextUtils" aboutText="About Us"/>
-      <TextForm heading ="Enter The Text To Analyze"/>
+      {/* <TextForm heading ="Enter The Text To Analyze"/> */}
+      <About/>
     </>
   );
 }
